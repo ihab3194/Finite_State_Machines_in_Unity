1ad534c7605c11e348c1184afe33209726b8b031
Ihab <mourad3180@hotmail.fr>
2020-01-20 12:22:31 -0500

    add finite state project

---
+++ Assets/Scripts/Player States/PlayerDuckingState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDuckingState : PlayerBaseState
+{
+    public override void EnterState(PlayerController_FSM player)
+    {
+        player.SetExpression(player.duckingSprite);
+        player.head.localPosition = new Vector3(player.head.localPosition.x, 0.5f, player.head.localPosition.z);
+    }
+
+    public override void Update(PlayerController_FSM player)
+    {
+        if (Input.GetButtonUp("Duck"))
+        {
+            player.head.localPosition = new Vector3(player.head.localPosition.x, 0.8f, player.head.localPosition.z);
+            player.TransitionToState(player.IdleState);
+
+        }
+    }
+
+    public override void OnCollisionEnter(PlayerController_FSM player)
+    {
+        throw new System.NotImplementedException();
+    }
+}
